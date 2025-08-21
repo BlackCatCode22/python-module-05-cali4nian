4901a82cfa9e053cf929f8bbf6f2816d9aae3b99
Daniel Soriano <danielsoriano559@gmail.com>
2025-08-21 08:57:36 -0700

    update countingWordFreq.py with comments

--- countingWordFreq.py
+++ countingWordFreq.py
@@ -1,7 +1,7 @@
 # Daniel Soriano
 
 fname = input("Enter file: ")
-if len(fname) < 1: fname = 'clown.txt'
+if len(fname) < 1: fname = 'clown.txt' # if no file name is provided
 hand = open(fname)
 
 di = dict()
@@ -10,12 +10,14 @@ for lin in hand:
     wds = lin.split()
     for w in wds:
         # idiom: retrieve/create/update counter
+        # looks for the word/key in the dictionary if not found value is 0
+        # else add one to the value if key is found
         di[w] = di.get(w,0) + 1
 
 # print(di)
 
 # now we want to find the most common word
-largest = -1
+largest = -1 # assigned -1 in case of zero being a number in the dictionary
 theword = None
 for k,v in di.items():
     print(k,v)
